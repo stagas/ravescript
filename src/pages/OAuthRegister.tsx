27cbbc0eaf5e7b1d1a6f24106463860bf881a86f
stagas <gstagas@gmail.com>
2024-10-04 10:22:12 +0300

    fix: make oauth/register nick input required

--- src/pages/OAuthRegister.tsx
+++ src/pages/OAuthRegister.tsx
@@ -33,7 +33,7 @@ export function OAuthRegister() {
         .then(() => go('/oauth/complete'))
         .catch(error => info.error = error.message)
     }}>
-      <input name="nick" value={() => info.nick} spellcheck="false" autocomplete="nickname" />
+      <input name="nick" value={() => info.nick} spellcheck="false" required autocomplete="nickname" />
       <button type="submit">Register</button>
       <br />
       {() => info.error}
