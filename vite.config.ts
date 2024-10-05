2f65868dbeae6dc0eaf40ef5dbb609c8a208ba8e
stagas <gstagas@gmail.com>
2024-10-06 02:22:43 +0300

    fix: pwa maybe now

--- vite.config.ts
+++ vite.config.ts
@@ -54,6 +54,25 @@ export default ({ mode }) => {
           short_name: 'Vasi',
           description: 'Vasi is a full-stack webapp base template using Deno and Vite.',
           theme_color: '#000000',
+
+          icons: [{
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          }, {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          }, {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }, {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          }],
         },
 
         injectManifest: {
